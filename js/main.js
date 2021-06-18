25f46acfe93a9677f216d937d05f4e7e344b2d68
niksvi <77024582+niksvi@users.noreply.github.com>
2021-06-18 19:41:15 +0400

    fix

--- js/main.js
+++ js/main.js
@@ -3,10 +3,9 @@ const MAX_COMMENT_LENGTH = 140;
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(min, max) {
   if (min >=0 && min < max){
-    min = Math.floor(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  } return 'Error @#$%^ !!!';
+    return Math.floor(Math.random() * (Math.floor(max) - Math.floor(min) + 1)) + Math.floor(min);
+  }
+  return 'Error @#$%^ !!!';
 }
 
 getRandomInt(15, 243);
