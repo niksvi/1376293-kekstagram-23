59e1a9ed05088e30e49b17cb6c31ecac63e77815
niksvi <77024582+niksvi@users.noreply.github.com>
2021-07-06 23:25:27 +0400

    ...

--- js/render-miniautures.js
+++ js/render-miniautures.js
@@ -4,11 +4,7 @@ const miniauturesList = document.querySelector('.pictures');
 
 const renderMiniatures = (photos) =>{
   const miniauturesListFragment = document.createDocumentFragment();
-<<<<<<< HEAD
   photos.forEach((photo, i) => {
-=======
-  photos.forEach((photo) => {
->>>>>>> 5e663b277f8f16ca573092b3779e2b6f2482e656
     const miniautureElement = miniautureTemplate.cloneNode(true);
     miniautureElement.querySelector('.picture__img').src = photo.url;
     miniautureElement.querySelector('.picture__img').dataset.image = i;
